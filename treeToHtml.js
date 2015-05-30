5c73550e96b43390f86ddadb875ab0a9df1998d6
mix irving <whimful@gmail.com>
2015-05-30 12:05:33 +1200

    fix nested tree stitching

--- treeToHtml.js
+++ treeToHtml.js
@@ -8,24 +8,43 @@ function treeToPlainHtml ( tree ) {
   var html = html || ''
   if (tree.parent == null) html = tree.content
 
-  tree.children.forEach( function(childTree) {
-    html = plainSubstitute( childTree.source, childTree.content, html )
-    treeToPlainHtml( childTree ) 
-  })
+  function recurssiveStitch(tree) {
+    tree.children.forEach( function(childTree) {
+      html = plainSubstitute( childTree.source, childTree.content, html )
+      recurssiveStitch( childTree ) 
+    })
+  }
+
+  recurssiveStitch(tree)
   return html
 }
+
+// TODO ask Mikey why this didn't work
+//function treeToStitchedHtml ( tree ) {
+  //var html = html || ''
+  //if (tree.parent == null) html = tree.content
+
+  //tree.children.forEach( function(childTree) {
+    //html = stitchSubstitute( childTree.source, childTree.content, html )
+    //treeToStitchedHtml( childTree ) 
+  //})
+  //return html
+//}
 
 function treeToStitchedHtml ( tree ) {
   var html = html || ''
   if (tree.parent == null) html = tree.content
 
-  tree.children.forEach( function(childTree) {
-    html = stitchSubstitute( childTree.source, childTree.content, html )
-    treeToStitchedHtml( childTree ) 
-  })
+  function recurssiveStitch(tree) {
+    tree.children.forEach( function(childTree) {
+      html = stitchSubstitute( childTree.source, childTree.content, html )
+      recurssiveStitch( childTree ) 
+    })
+  }
+
+  recurssiveStitch(tree)
   return html
 }
-
 function plainSubstitute (url, importedText, wholeText) {
   var regex = new RegExp('\\+\<a href\=(\'|\")' + url + '.*\<\/a\>', 'g')
   return wholeText.replace(regex, importedText)
